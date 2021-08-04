3299773ef9b6f9420894690e6c8156e5fa50684d
renatafreitas <68546902+renatafreitas@users.noreply.github.com>
2021-08-04 10:01:18 +0100

    added changes to sidekiq

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
-class MovieController < ApplicationController
+class MoviesController < ApplicationController
   def show
-    @movie = Movie.fidn
+    movie = OmdbMovie.find_movie_or_call_it(params[:title])
+    if movie
+      render json: movie
+    end
   end
 end
