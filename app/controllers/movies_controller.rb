d74615e2562e644852f0533c1df9ccf295999563
renatafreitas <68546902+renatafreitas@users.noreply.github.com>
2021-11-27 12:09:03 +0000

    add view to movies infex

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
   def show
     movie = OmdbMovie.find_movie_or_call_it(params[:title])
     if movie
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => "/sidekiq"
-
-  resources :movies, only: %i[show], param: :title
+  get "/movies", to: "movies#index"
+  resources :movies, only: %i[index, show], param: :title
 end
