ac7691456a2ace27a7a501ee2314fc51bdfb4c39
renatafreitas <68546902+renatafreitas@users.noreply.github.com>
2021-11-30 12:13:08 +0000

    chande the routes root to movie index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => "/sidekiq"
+  root :to => "movies#index"
   get "/movies", to: "movies#index"
   resources :movies, only: %i[index, show], param: :title
 end
